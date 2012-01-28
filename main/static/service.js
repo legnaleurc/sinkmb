2d807d716dfdd313d38a06e781c54a6c1bed2f0d
Wei-Cheng Pan <legnaleurc@gmail.com>
2012-01-28 20:08:03 +0800

    Add comment.

--- main/static/service.js
+++ main/static/service.js
@@ -1,14 +1,30 @@
 ( function() {
 
+	/**
+	 * @brief Abstract class for services
+	 */
 	function Service() {
 		this.oldPostQueue = [];
 		this.pulling = false;
 	}
 
+	/**
+	 * @brief Render @p data to a jQuery DOM node
+	 * @param data The object to render
+	 * @return a jQuery object
+	 *
+	 * The jQuery object MUST have data, which key is 'timestamp', value is a Date object.
+	 */
 	Service.prototype.renderPost = function( data ) {
 		throw new TypeError( 'pure virtual function call' );
 	};
 
+	/**
+	 * @brief Retrives new posts
+	 * @return Service object
+	 *
+	 * If there is another job exists, this method does nothing.
+	 */
 	Service.prototype.update = function() {
 		if( this.pulling ) {
 			return this;
@@ -17,10 +33,23 @@
 		return this;
 	};
 
+	/**
+	 * @brief Retrives new posts
+	 * @return undefined
+	 * @todo Rename this method, this is ambiguous
+	 *
+	 * This method could be asynchronized.
+	 */
 	Service.prototype.pullNew = function() {
 		throw new TypeError( 'pure virtual function call' );
 	};
 
+	/**
+	 * @brief Get the top-must (i.e. latest) post
+	 * @return Post object, or null if empty
+	 *
+	 * This method will pull historical post if needed. It will not pull again if already pulling.
+	 */
 	Service.prototype.topPost = function() {
 		if( this.oldPostQueue.length > 0 ) {
 			return this.oldPostQueue[0];
@@ -32,6 +61,10 @@
 		return null;
 	};
 
+	/**
+	 * @brief Pop the top-must (i.e. latest) post
+	 * @return Post object, or null if empty
+	 */
 	Service.prototype.popPost = function() {
 		if( this.oldPostQueue.length > 0 ) {
 			return this.oldPostQueue.shift();
